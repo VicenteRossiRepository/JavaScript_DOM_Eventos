90a05f78cfd8122dee32d100fb9c1eb156e87d71
Vicente <vicente.rossi.contacto@gmail.com>
2024-11-28 21:17:02 -0300

    Modificacion de clase scripts.js de querySelectorAll y GetElementById

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,16 @@ heading.textContent = 'Nuevo Heading'
 console.log(heading)
 
 //querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a')
+console.log(enlaces)
 
+enlaces[0].textContent = 'Nuevo texto para enlaces'
+
+enlaces[0].href = 'http://google.com'
+enlaces[0].classList.add('nueva-clase')
+// enlaces[0].classList.remove('navegacion__enlace')
 
 //GetElementById
+
+const heading2 = document.getElementById('heading')
+console.log(heading2)
