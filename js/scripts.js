b6e03f3feb1c5f35c37b332faeba864bdabbb248
Vicente <vicente.rossi.contacto@gmail.com>
2024-12-03 20:40:31 -0300

    Modificacion de clase scripts.js de eventos de input

--- js/scripts.js
+++ js/scripts.js
@@ -34,25 +34,25 @@ console.log(nuevoEnlace)
 
 
 //Eventos
-console.log(1)
+// console.log(1)
 
-window.onload = function (){
-    console.log(3)
-}
-window.addEventListener('load', function (){ //load espera a que el JS y los archivos que dependen del HTML esten listos
-    console.log(2)
-});
+// window.onload = function (){
+//     console.log(3)
+// }
+// window.addEventListener('load', function (){ //load espera a que el JS y los archivos que dependen del HTML esten listos
+//     console.log(2)
+// });
 
-document.addEventListener('DOMContentLoaded', function(){ //DOMContentLoaded solo espera que se carge el HTML y nada mas
-    console.log(4)
-})
+// document.addEventListener('DOMContentLoaded', function(){ //DOMContentLoaded solo espera que se carge el HTML y nada mas
+//     console.log(4)
+// })
 
-console.log(5)
+// console.log(5)
 
 
-window.onscroll = function(){
-    console.log('Scroll')
-}
+// window.onscroll = function(){
+//     console.log('Scroll')
+// }
 
 
 //Seleccionar elementos y asociar eventos
@@ -62,3 +62,25 @@ btnEnviar.addEventListener('click',function(e) {
     e.preventDefault(); //Previene el comportamiento por defecto del evento; sirve para validar formularios
     console.log('Botón enviado');
 });
+
+
+
+// Eventos inputs y textAreas
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const mail = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+nombre.addEventListener('input', leerTexto);
+mail.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e){
+    datos[e.target.id] = e.target.value;
+    console.log(datos)
+}
